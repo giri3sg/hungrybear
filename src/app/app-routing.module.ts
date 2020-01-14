f2ef2435051edaf5fc598245f22aa85a2a2b155f
Girish Sivadanam <girish.sivadanam@conweaver.com>
2020-01-14 14:05:08 +0100

    added home and dashboard components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, GuardsCheckStart} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppComponent} from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
